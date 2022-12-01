50f9e0eed232dcd619a6f6de9d5cf2c28c08b1eb
johnsalliaris <john.salliaris@amido.com>
2022-12-01 16:55:53 +0000

    fmt

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -3,29 +3,31 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 pub fn main() {
-
-    let snacky_elves:Vec<i32> = parse_input("./resources/day1/input");
+    let snacky_elves: Vec<i32> = parse_input("./resources/day1/input");
     calculate_top_3_snackers(&snacky_elves);
 }
 
-fn calculate_top_3_snackers(snacky_elves: &Vec<i32>){
+fn calculate_top_3_snackers(snacky_elves: &Vec<i32>) {
     let max_calories = calculate_max_calories(snacky_elves);
     let mut second_max_calories = 0;
     let mut third_max_calories = 0;
 
     for x in snacky_elves {
-        if x >= &second_max_calories && x< &max_calories {
+        if x >= &second_max_calories && x < &max_calories {
             second_max_calories = *x;
         }
 
-        if x >= &third_max_calories && x< &second_max_calories {
+        if x >= &third_max_calories && x < &second_max_calories {
             third_max_calories = *x;
         }
     }
 
     println!("second most calories {}", second_max_calories);
     println!("third most calories {}", third_max_calories);
-    println!("top 3 calories in total {}", max_calories + second_max_calories + third_max_calories);
+    println!(
+        "top 3 calories in total {}",
+        max_calories + second_max_calories + third_max_calories
+    );
 }
 
 fn calculate_max_calories(snacky_elves: &Vec<i32>) -> i32 {
@@ -42,7 +44,7 @@ fn calculate_max_calories(snacky_elves: &Vec<i32>) -> i32 {
     return max_calories;
 }
 
-fn parse_input(x: &str) -> Vec<i32>{
+fn parse_input(x: &str) -> Vec<i32> {
     let mut vector: Vec<i32> = Vec::new();
     vector.push(0);
     let mut last_index = 0;
@@ -64,9 +66,10 @@ fn parse_input(x: &str) -> Vec<i32>{
     return vector;
 }
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
-
-
